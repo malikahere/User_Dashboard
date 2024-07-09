5cbc577c59063ef15d715db44af51ab504106f0f
Malika Garg <malikagarg121@gmail.com>
2024-07-09 20:31:42 +0530

    updated

--- user_dashboard/settings.py
+++ user_dashboard/settings.py
@@ -18,7 +18,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 
