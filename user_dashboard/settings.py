ecea84f1b38fe18e3c0c49c3557075040c0143bd
Malika Garg <malikagarg121@gmail.com>
2024-07-09 20:46:44 +0530

    updated statics

--- user_dashboard/settings.py
+++ user_dashboard/settings.py
@@ -17,8 +17,9 @@ from pathlib import Path
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+TEMPLATES_DIR = os.path.join(BASE_DIR,'accounts', 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'accounts', 'static')
+
 
 
 
@@ -63,7 +64,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 # user_dashboard/settings.py
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 TEMPLATES = [
@@ -132,10 +133,9 @@ USE_TZ = True
 import os
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR]
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Add this if not already included to serve static files during development
